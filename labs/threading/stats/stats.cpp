18f565fbb9c5be29d2f588ed5119d3a63dd6e22a
Christian DeLozier <delozier@WRK1060057GOVT.academy.usna.edu>
2024-02-16 07:47:49 -0500

    Adding updated file from in-class threading demo

--- labs/threading/stats/stats.cpp
+++ labs/threading/stats/stats.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <atomic>
 
 using namespace std;
 
@@ -21,6 +23,31 @@ vector<int> readFile(string fileName){
   return values;
 }
 
+void doSum(vector<int> values, int size, int start, atomic<long> *sum){
+  for(int i = start; i < size; i++){
+    *sum += values[i];
+  }  
+}
+
+void doStats(char *fileName){
+  vector<int> values = readFile(fileName);
+  
+  atomic<long> sum(0);
+  if(values.size() > 1000){
+    thread t1(doSum,values,values.size()/2,0,&sum);
+    thread t2(doSum,values,values.size(),values.size()/2,&sum);
+    t1.join();
+    t2.join();
+  }else{
+    doSum(values, values.size(), 0, &sum);
+  }
+  
+  sort(values.begin(),values.end());
+  
+  cout << fileName << " - " << sum/values.size() << " - "
+       << values[values.size()/2] << "\n";  
+}
+
 int main(int argc, char **argv){
   if(argc < 2){
     cout << "Usage: " << argv[0] << " file1 [file2] ...\n";
@@ -28,18 +55,17 @@ int main(int argc, char **argv){
   }
 
   cout << "File - Average - Median\n";
+
+  vector<thread*> threads;
   
   for(int c = 1; c < argc; c++){
-    vector<int> values = readFile(argv[c]);
+    thread *t = new thread(doStats,argv[c]);
+    //doStats(argv[c]);
+    threads.push_back(t);
+  }
 
-    long sum = 0;
-    for(int i = 0; i < values.size(); i++){
-      sum += values[i];
-    }
-
-    sort(values.begin(),values.end());
-
-    cout << argv[c] << " - " << sum/values.size() << " - " << values[values.size()/2] << "\n";
+  for(int c = 1; c < argc; c++){
+    threads[c-1]->join();
   }
   
   return 0;
